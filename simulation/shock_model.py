9ad0cc7d395d30fed86d149e1acb00ba1d214920
Manuel Magnabosco <magn.manuel05@gmail.com>
2026-03-14 16:07:28 +0100

    nuova bibliografia

--- simulation/shock_model.py
+++ simulation/shock_model.py
@@ -1,4 +1,4 @@
-import scipy as sp
+from scipy.integrate import solve_ivp
 import numpy as np
 import pandas as pd
 
@@ -7,7 +7,14 @@ def read_data(file):
     df = pd.read_csv(file)
     return df[['id', 'monthly_income_hist', 'monthly_fixed_exp_hist', 'monthly_variable_exp_hist', 'monthly_saving_hist', 'debt_hist', 'bnpl_exposure']]
 
+def shock(df): 
+    #parametri fissi 
+    INTEREST_RATE=0.015
+    T_MAX=12
+    N_STEPS=200
 
+    #ODE
+    income
 
 
 
